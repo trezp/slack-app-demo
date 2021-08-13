bb4540266c7d425a91fd69554ef7757075cba08b
Treasure Porth <treasure.rose@gmail.com>
2021-08-13 15:25:43 -0700

    initial boilerplate

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const { App } = require('@slack/bolt');
+
+//Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
